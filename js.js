9ac26b0d2b9924290489f35f895914e1b761bc28
Omar Hashimi <omar.hashime786@gmail.com>
2023-09-13 14:47:20 +0430

    finished the Intro part Ready to work on Work section

--- js.js
+++ js.js
@@ -1,2 +1,14 @@
 
 
+let x = false;
+function menu(){
+    if(x==false){
+        x = !x;
+        document.getElementById("menu").style.right = "0"
+
+    }else {
+        x = !x;
+        document.getElementById("menu").style.right = "-280px"
+    }
+}
+
