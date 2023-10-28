659620ddd5ba5e17f7cae2bfb9edc459443cc700
Omar Hashimi <omar.hashime786@gmail.com>
2023-10-28 20:56:10 +0430

    add the footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,15 @@
+import "./footer.scss";
+
+function Footer() {
+  return (
+    <div className="footer">
+
+        <div className="contain">
+            <h1>Developed By <span>Omar Hashimi</span></h1>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Footer
